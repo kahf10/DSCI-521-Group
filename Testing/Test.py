649bb839b02880123971dd93e6ceb9c449c372d1
Kahf Hussain <kahfnhussain@gmail.com>
2025-03-07 17:01:38 -0500

    Finished Module A and till B11

---
+++ Testing/Test.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+transactions = pd.read_csv('../data/preprocessed_transactions_data.csv')
+users = pd.read_csv('../data/preprocessed_users_data.csv')
+
+pd.set_option('display.max_columns', None)  # Show all columns
+pd.set_option('display.max_colwidth', None)  # Remove column width restriction
+
+print(users.head())
+print(len(users))
+
+print(transactions.sample(10))
+print(users.sample(10))
+
+print(transactions['use_chip'].unique())
